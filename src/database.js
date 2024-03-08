32bc32e92383103d74b7088ac9538d75d2be9f0e
Wellington Albino <well.renato@hotmail.com>
2024-03-07 22:07:37 -0300

    Finalizando projeto

--- src/database.js
+++ src/database.js
@@ -18,8 +18,16 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database))
   }
 
-  select(table) {
-    const data = this.#database[table] ?? []
+  select(table, search) {
+    let data = this.#database[table] ?? []
+
+    if(search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
 
     return data
   }
@@ -36,4 +44,21 @@ export class Database {
     return data
   }
 
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][tableRow] = { id, ...data }
+      this.#persist()
+    }
+  }
 }
